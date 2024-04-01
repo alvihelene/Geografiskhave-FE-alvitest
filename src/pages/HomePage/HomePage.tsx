520139ddcaf87fd299c23bda3035792c24764d6c
Artaey <95622783+Artaey@users.noreply.github.com>
2024-04-01 12:47:57 +0200

    globals + varibles in scss made woohoo...
    Co-Authored-By: alvihelene <95623140+alvihelene@users.noreply.github.com>

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { observer } from "mobx-react-lite";
+import "./HomePage.scss";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return <h1>HomePage</h1>;
 };
 
 export default observer(HomePage);
