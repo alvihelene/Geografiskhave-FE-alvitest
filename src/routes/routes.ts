881b972894a803dee511647541e50af7ea230463
Mike Hovedskov <deengo113@gmail.com>
2024-04-08 21:46:37 +0200

    Added test page for test of components

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,8 +2,8 @@ import { IRoute, RouteType } from "../interfaces/IRoute";
 import HomePage from "../pages/HomePage/HomePage.tsx";
 import home from "../assets/icons/home.svg";
 import map from "../assets/icons/map.svg";
-import logout from "../assets/icons/logout.svg";
 import profile from "../assets/icons/user.svg";
+import TestPage from "../pages/TestPage/TestPage.tsx";
 import placeholder from "../assets/icons/placeholder.svg";
 import ProfilePage from "../pages/ProfilePage/ProfilePage.tsx";
 
@@ -35,6 +35,13 @@ export const Routes: IRoute[] = [
     icon: profile,
     component: ProfilePage,
     routeType: RouteType.NAVBAR,
+  },
+  //TODO: REMOVE THIS IN PRODUCTION
+  {
+    path: "/test",
+    title: "Test",
+    routeType: RouteType.INTERNAL,
+    component: TestPage,
   },
 ];
 
