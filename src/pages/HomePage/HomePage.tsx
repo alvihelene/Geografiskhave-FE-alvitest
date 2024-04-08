dd153b1be534fa9280bd9ec17a81ecb865f76e42
Mike Hovedskov <deengo113@gmail.com>
2024-04-08 21:42:19 +0200

    removed profile from homepage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { observer } from "mobx-react-lite";
 import "./HomePage.scss";
-import Profile from "../../components/profile/profile/profile";
 const HomePage = () => {
   return (
     <div className="HomePage">
       <h1>Home Page</h1>
-      <Profile />
     </div>
   );
 };
