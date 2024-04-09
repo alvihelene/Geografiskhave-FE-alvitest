9a2bdbf68a62f51b8c7ad7472fc87a5e3049bdf8
Artaey <95622783+Artaey@users.noreply.github.com>
2024-04-09 21:31:18 +0200

    add buttons
    props used to change text, color, size

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import { observer } from "mobx-react-lite";
 import "./HomePage.scss";
+
+import Button from "../../components/shared/buttons/button";
+
 const HomePage = () => {
   return (
     <div className="HomePage">
       <h1>Home Page</h1>
+      <Button 
+      text="kanp"
+      size="small"
+      color="white"
+      />
+      <Button 
+      text="button"
+      size="large"
+      color="orange"
+      />
     </div>
   );
 };
