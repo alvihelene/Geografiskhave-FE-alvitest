c4bc929a85649e439145fa735edb58e0ff8954a0
alvihelene <95623140+alvihelene@users.noreply.github.com>
2024-04-10 09:50:10 +0200

    Header - unnecessary code removed in header

--- src/components/shared/header/header.tsx
+++ src/components/shared/header/header.tsx
@@ -7,30 +7,10 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ currentPage }) => {
-  const getTime = () => {
-    const date = new Date();
-    return `${date.getHours()}:${date.getMinutes()}`;
-  };
 
   return (
     <div className="header">
-    <div className="top-container">
-        <div className="left-container">
-            <div className="left">
-            <span>{getTime()}</span>
-            </div>
-        </div>
-        <div className="right-container">
-            <div className="right">
-                <span className="icon internet"></span>
-                <span className="icon wifi"></span>
-                <span className="icon battery"></span>
-            </div>
-        </div>
-      </div>
-      <div className="bottom-container">
-        <div>{currentPage}</div> {/* This is under the right and left divs */}
-      </div>
+        <div>{currentPage}</div> 
     </div>
   );
 };
