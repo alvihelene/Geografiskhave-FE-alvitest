248df5dae20f5ef9b540ff0ffa8c1c041485efe3
alvihelene <95623140+alvihelene@users.noreply.github.com>
2024-04-10 16:55:30 +0200

    Header component - "<-" and "X" divs added. Not functional yet.

--- src/components/shared/header/header.tsx
+++ src/components/shared/header/header.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import "./header.scss"
 
-
 interface HeaderProps {
   currentPage: string;
 }
 
 const Header: React.FC<HeaderProps> = ({ currentPage }) => {
-
   return (
     <div className="header">
-        <div>{currentPage}</div> 
+      <div className="left-icon">
+        {currentPage !== "firstPage" && <p>{"<-"}</p>}
+      </div>
+      <div className="currentPage">{currentPage}</div>
+      <div className="right-icon">
+        {currentPage !== "lastPage" && <p>X</p>}
+      </div>
     </div>
   );
 };
 
 export default Header;
-
