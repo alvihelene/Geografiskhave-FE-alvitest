8fa31282814c7d01bbf5b6a28b2c34171e2b1749
alvihelene <95623140+alvihelene@users.noreply.github.com>
2024-04-11 12:55:37 +0200

    Inputfields done
    Co-Authored-By: Mike Handberg Hovedskov <50768854+Frann0@users.noreply.github.com>

--- src/pages/TestPage/TestPage.tsx
+++ src/pages/TestPage/TestPage.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Input from "../../components/shared/inputField/input";
+import backIcon from "../../assets/icons/backIcon.svg";
 
 const TestPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Input
+        placeholder={"Password"}
+        onChange={(e) => console.log(e.target.value)}
+        icon={backIcon}
+      />
+    </div>
+  );
 };
 
 export default TestPage;
