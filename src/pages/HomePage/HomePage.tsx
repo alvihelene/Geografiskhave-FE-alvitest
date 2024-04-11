77cb2aae923fe48c269fda75c1e90843538e3f36
Artaey <95622783+Artaey@users.noreply.github.com>
2024-04-11 11:26:58 +0200

    removed test on homepage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { observer } from "mobx-react-lite";
 import "./HomePage.scss";
-import Splash from "../../components/spalsh/splashThing";
 
 const HomePage = () => {
   return (
