08fce8004874c45b29973b88320270bfa6c6f152
Mike Hovedskov <deengo113@gmail.com>
2024-04-11 09:55:48 +0200

    Fixed routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.scss";
 import { observer } from "mobx-react-lite";
-import { Routes as routes, navRoutes } from "./routes/routes";
-import { Routes, Route, Outlet, useLocation } from "react-router-dom";
+import { navRoutes } from "./routes/routes";
+import { Outlet, useLocation } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import Navbar from "./components/shared/navbar/navbar";
 import { routeHasNavbar } from "./utils/RouteUtil";
