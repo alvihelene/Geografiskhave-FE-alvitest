5ca8168a7e262681e84e3c97d9970b8c0c83b9d8
Artaey <95622783+Artaey@users.noreply.github.com>
2024-04-11 11:23:58 +0200

    splash fades out after a short time

--- src/components/spalsh/splashThing.tsx
+++ src/components/spalsh/splashThing.tsx
@@ -1,13 +1,26 @@
 import "./splash.scss";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Logo from "../../assets/logo/Geografisk_Have_Logo.svg"
 
 const Splash = () => {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    // Simulate loading time
+    const timeout = setTimeout(() => {
+      setLoaded(true);
+    }, 1000); // Adjust the time as needed
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
-    <div className="Splash">
+    <div className={`Splash ${loaded ? 'loaded' : ''}`}>
         <img className="logo" src={Logo} alt="Geografisk Have Logo" />
     </div>
   );
 };
+
+
 
 export default Splash;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -6,7 +6,6 @@ import Splash from "../../components/spalsh/splashThing";
 const HomePage = () => {
   return (
     <div className="HomePage">
-      <Splash></Splash>
       <h1>Home Page</h1>
     </div>
   );
