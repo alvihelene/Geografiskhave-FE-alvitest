9d377320b2ac5fed5642737a4124cd5660d0de02
alvihelene <95623140+alvihelene@users.noreply.github.com>
2024-04-12 15:03:15 +0200

    Treasure hunt component done

--- src/components/shared/treasureHuntHomePage/treasureHunt.tsx
+++ src/components/shared/treasureHuntHomePage/treasureHunt.tsx
@@ -8,15 +8,31 @@ const TreasureHunt = () => {
 
         <div className="TreasureHunt_ImageContainer">
           <div className="TreasureHunt_InfoContainer">
-            <p className="TreasureHunt_InfoContainer_Header">
+            <div className="TreasureHunt_InfoContainer_Header">
               Geografisk eventyr
-            </p>
+            </div>
             <div className="TreasureHunt_InfoContainer_Content">
               <div className="Left1">
-                <p>Opgave 4 |</p>
+                <p className="TreasureHunt_InfoContainer_Content_Text">
+                  Opgave 4 |{" "}
+                </p>
               </div>
               <div className="Right1">
-                <p>Naturhaven</p>
+                <p className="TreasureHunt_InfoContainer_Content_Text">
+                  Naturhaven
+                </p>
+              </div>
+            </div>
+            <div className="TreasureHunt_InfoContainer_Progress">
+              <div className="Progressbar">
+                <div className="Line">
+                  <div className="Line_Taskprogress"></div>
+
+                </div>
+                <div className="TasksProgress">4/6</div>
+              </div>
+              <div className="TreasureHunt_BackButton">
+                <img src="./src/assets/icons/edit.svg" alt="Back Arrow" />
               </div>
             </div>
           </div>
