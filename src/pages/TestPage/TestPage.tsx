1033a158255994d53975a2e1dcc084e7bfaea8f6
Artaey <95622783+Artaey@users.noreply.github.com>
2024-04-17 21:33:53 +0200

    fixed width

--- src/pages/TestPage/TestPage.tsx
+++ src/pages/TestPage/TestPage.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./TestPage.scss"
+import Card from "../../components/shared/cards/card"
+import Ticket from "../../assets/icons/ticket.svg"
 
 const TestPage = () => {
-  return <div></div>;
+  return <div className="test">
+    <Card
+    text="bob"
+    icon={Ticket}
+    link=""
+    />
+    <Card
+    text="bob"
+    icon={Ticket}
+    link=""
+    />
+  </div>;
 };
 
 export default TestPage;
