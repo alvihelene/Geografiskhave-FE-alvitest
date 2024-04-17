97c1ce09f7c66bb47b3c76e5a2b8060c9df9d2ff
Artaey <95622783+Artaey@users.noreply.github.com>
2024-04-17 21:08:18 +0200

    Start of front page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { observer } from "mobx-react-lite";
 import "./HomePage.scss";
+import FrontpageProfile from "../../components/profile/frontpageProfile/frontpageProfile";
+import TreasureHunt from "../../components/shared/treasureHuntHomePage/treasureHunt";
+
 const HomePage = () => {
   return (
     <div className="HomePage">
-      <h1>Home Page</h1>
+      <FrontpageProfile/>
+      <TreasureHunt/>
     </div>
   );
 };
