b59af9803a0dc3b8a7210c092a20821a8daec831
Artaey <95622783+Artaey@users.noreply.github.com>
2024-04-17 20:39:54 +0200

    renoved stuff from test page

--- src/pages/TestPage/TestPage.tsx
+++ src/pages/TestPage/TestPage.tsx
@@ -1,23 +1,8 @@
 import React from "react";
 import "./TestPage.scss"
-import Card from "../../components/shared/cards/card"
-import Button from "../../components/shared/buttons/button";
-import icon  from "../../assets/icons/placeholder.svg"
 
 const TestPage = () => {
-  return <div>
-    <div className="test">
-      <Card
-      text="bob"
-      icon={icon}
-      link="https://www.w3schools.com/html/html_links.asp"/>
-      <Card
-      text="tob"
-      icon={icon}
-      link=""/>
-    </div>
-    
-  </div>;
+  return <div></div>;
 };
 
 export default TestPage;
